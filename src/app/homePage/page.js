901033a2465cedc300dc8aaf86b98b697044ee0c
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-05-08 19:46:04 +0530

    user info successfully getting in HomePage

--- src/app/homePage/page.js
+++ src/app/homePage/page.js
@@ -6,6 +6,16 @@ import { getAuth, signOut } from "firebase/auth";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+  
+
 export default function HomePage(){
 
     const router = useRouter();
@@ -20,12 +30,19 @@ export default function HomePage(){
         }
     }
     return(
-        <main>
-            <h1>{`Welcome :${user?.displayName}`}</h1>
-            <p>{`Email: ${user?.email}`}</p>
-            <img className="rounded-full shadow-xl w-10 h-10" src={user.photoURL}/>
-
-            <Button variant="destructive" onClick={logOut}>Log Out</Button>
+        <main className="select-none min-h-screen w-full p-3">
+            <DropdownMenu>
+            <DropdownMenuTrigger className=" rounded-xl border-2 border-gray-600 px-4 py-2">My Profile</DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>{user?.displayName}</DropdownMenuLabel>
+                <DropdownMenuSeparator/>
+                <DropdownMenuItem><img src={user?.photoURL} className="rounded-full w-10 h-10"/></DropdownMenuItem>
+                <DropdownMenuSeparator/>
+                <DropdownMenuItem>{user?.email}</DropdownMenuItem>
+                <DropdownMenuSeparator/>
+                <DropdownMenuItem><Button variant="destructive" onClick={logOut}>Log out</Button></DropdownMenuItem>
+            </DropdownMenuContent>
+            </DropdownMenu>
         </main>
     )
 }
